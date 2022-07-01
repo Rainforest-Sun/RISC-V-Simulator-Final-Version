0a4bb5a1663a1f3c4a09c988313dae0dca094c7a
rainforest <rain-forest@sjtu.edu.cn>
2022-07-01 16:52:12 +0800

    many files

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 
 int main()
 {
-    freopen("C:\\Users\\rainforest\\Desktop\\testcases\\bulgarian.data", "r", stdin);
-    freopen("C:\\Users\\rainforest\\Desktop\\out.txt", "w", stdout);
     read_order();
     run_order();
     printf("%u\n",reg[10]&255u);
